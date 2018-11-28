ea0ce1898e1dd28e4b8f3af532a491402a0e20b6
Nicholas Tyler <tylern@canisius.edu>
2018-11-28 08:47:14 -0500

    Fix for init in python

--- Banks/clas12event.h
+++ Banks/clas12event.h
@@ -14,77 +14,66 @@
 #ifndef CLAS12_EVENT_H
 #define CLAS12_EVENT_H
 
-#include <iostream>
-#include <cstdlib>
+#include "detector.h"
+#include "header.h"
+#include "particle.h"
+#include "vectors.h"
 #include <cstdio>
-#include <string>
-#include <vector>
+#include <cstdlib>
+#include <iostream>
 #include <stdint.h>
 #include <stdlib.h>
-#include "particle.h"
-#include "detector.h"
-#include "vectors.h"
-#include "header.h"
-
+#include <string>
+#include <vector>
 
 namespace clas12 {
 
-  static const double FTOF = 12;
-  static const double HTCC = 15;
-  static const double EC   = 7;
+static const double FTOF = 12;
+static const double HTCC = 15;
+static const double EC = 7;
 
+static const double FTOF1A = 121;
+static const double FTOF1B = 122;
+static const double PCAL = 71;
+static const double ECIN = 72;
+static const double ECOUT = 73;
 
-  static const double FTOF1A = 121;
-  static const double FTOF1B = 122;
-  static const double PCAL   = 71;
-  static const double ECIN   = 72;
-  static const double ECOUT  = 73;
+class clas12event {
 
-  class clas12event {
-
-  private:
-
+private:
   /*static const char *vinit[] = {
       "REC::Event","REC::Particle","REC::Calorimeter",
       "REC::Scintillator" };
 */
-    std::vector<std::string> banks;
+  std::vector<std::string> banks = {"REC::Event", "REC::Particle",
+                                    "REC::Calorimeter", "REC::Scintillator"};
+  clas12::header clas12header;
+  clas12::particle clas12particle;
+  clas12::detector clas12calorimeter;
+  clas12::detector clas12tof;
+  clas12::detector clas12cherenkov;
 
-    clas12::header     clas12header;
-    clas12::particle   clas12particle;
-    clas12::detector   clas12calorimeter;
-    clas12::detector   clas12tof;
-    clas12::detector   clas12cherenkov;
+public:
+  clas12event() {}
 
-  public:
+  clas12event(hipo::reader &r) { init(r); }
 
-    clas12event(){ }
+  ~clas12event() {}
 
-    clas12event(hipo::reader &r) {
-      banks.push_back("REC::Event");
-      banks.push_back("REC::Particle");
-      banks.push_back("REC::Calorimeter");
-      banks.push_back("REC::Scintillator");
-      init(r);
-    }
+  void init(hipo::reader &r);
 
-    ~clas12event(){ }
+  double getStartTime();
+  double getTime(int detector, int pindex);
+  double getEnergy(int detector, int pindex);
+  double getPath(int detector, int pindex);
+  double getBeta(int detector, int pindex);
+  void getHitPosition(int detector, int pindex, vector3 &vec);
+  void getHitPositionLocal(int detector, int pindex, vector3 &vec);
 
-    void    init(hipo::reader &r);
+  clas12::particle &particles() { return clas12particle; }
+  clas12::header &header() { return clas12header; }
+}; // namespace clas12
 
-    double  getStartTime();
-    double  getTime  ( int detector, int pindex);
-    double  getEnergy( int detector, int pindex);
-    double  getPath(   int detector, int pindex);
-    double  getBeta(   int detector, int pindex);
-    void    getHitPosition(int detector, int pindex, vector3 &vec);
-    void    getHitPositionLocal(int detector, int pindex, vector3 &vec);
-
-    clas12::particle  &particles(){return clas12particle;}
-    clas12::header    &header(){ return clas12header;}
-
-  };
-
-}
+} // namespace clas12
 
 #endif /* UTILS_H */
