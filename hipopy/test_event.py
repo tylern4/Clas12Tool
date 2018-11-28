ca4a6fc26e0e312dc845a59b2d8e5596ed11a6c5
Nicholas Tyler <tylern@canisius.edu>
2018-11-28 17:12:27 -0500

    Update with proc function

--- hipopy/test_event.py
+++ hipopy/test_event.py
@@ -12,6 +12,9 @@ from ROOT import TLorentzVector, TH2D, TFile
 MASS_P = 0.93827
 c_special_units = 29.9792458
 
+delta_t = TH2D( 'delta_t', '#Delta T Protons', 500, 0.0, 7.0, 500, -10, 10)
+hfile = TFile( 'example.root', 'RECREATE')
+
 def vertex_time(sc_time, sc_pathlength, relatavistic_beta):
 	return sc_time - sc_pathlength / (relatavistic_beta * c_special_units)
 
@@ -20,29 +23,32 @@ def calc_dt(vertex, momentum, sc_t, sc_r, mass):
     beta = 1.0 / np.sqrt(1.0 + mp * mp)
     return vertex - vertex_time(sc_t, sc_r, beta)
 
-
-
-delta_t = TH2D( 'delta_t', '#Delta T Protons', 500, 0.0, 7.0, 500, -10, 10)
-hfile = TFile( 'example.root', 'RECREATE')
-
-start = time.time()
-i = 0
-for file_name in sys.argv[1:]:
+def proc(file_name):
+	i = 0
 	reader = hipo_reader(file_name)
 	data = Event(reader)
 	for event in data:
 		i += 1
-    	sc_r_vertex = event.getPath(detector['FTOF1A'], 0)
-    	sc_t_vertex = event.getTime(detector['FTOF1A'], 0)
+		sc_r_vertex = event.getPath(detector['FTOF1B'], 0)
+		sc_t_vertex = event.getTime(detector['FTOF1B'], 0)
 
-    	vertex = vertex_time(sc_t_vertex, sc_r_vertex, 1.0)
-    	for x in range(1, len(event)):
-        	momentum = event.particle[x].P
-        	if momentum == 0:
+		vertex = vertex_time(sc_t_vertex, sc_r_vertex, 1.0)
+		for x in range(1, len(event)):
+			momentum = event.particle[x].P
+			if momentum == 0:
 				continue
-        	sc_r = event.getPath(detector['FTOF1A'], x)
-        	sc_t = event.getTime(detector['FTOF1A'], x)
-        	delta_t.Fill(momentum, calc_dt(vertex, momentum, sc_t, sc_r, MASS_P))
+			sc_r = event.getPath(detector['FTOF1B'], x)
+			sc_t = event.getTime(detector['FTOF1B'], x)
+			delta_t.Fill(momentum, calc_dt(vertex, momentum, sc_t, sc_r, MASS_P))
+	return i
+
+
+
+start = time.time()
+i = 0
+for file_name in sys.argv[1:]:
+	i += proc(file_name)
+	print((i / (time.time() - start)), "Hz")
 
 end = time.time()
 print((end - start), "Sec")
